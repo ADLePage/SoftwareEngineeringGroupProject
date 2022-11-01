44dbf5c592a93fca37ac8ced15d365a892ced98c
ikotu002 <ikotu002@d.umn.edu>
2022-11-01 14:36:20 -0500

    ALMOST WITH THE JOURNAL PAGE

--- app/src/main/java/com/example/baads/journalPage.java
+++ app/src/main/java/com/example/baads/journalPage.java
@@ -16,7 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class journalPage extends AppCompatActivity {
-     Button newButton,saveButton,openButton;
+     Button newButton, saveButton,openButton;
      EditText editText;
 
     @Override
@@ -28,29 +28,21 @@ public class journalPage extends AppCompatActivity {
         saveButton = (Button)findViewById(R.id.saveButton);
         openButton = (Button)findViewById(R.id.openButton);
         editText = (EditText)findViewById(R.id.text);
-/*
-        saveButton.setOnClickListener(e->saveButtonFunction());
-        saveButton.setText("Save Button");
-        //saveButton.
 
- */
     }
-
-
 
     public void buttonAction(View wow) {
         final EditText fileName = new EditText(this);
         AlertDialog.Builder ad = new AlertDialog.Builder(this);
         ad.setView(fileName);
 
-        if (wow.getId() == R.id.saveButton)
-        {
-            ad.setMessage("SAVE TODAY'S JOURNAL");
+        if (wow.getId() == R.id.saveButton) {
+            ad.setMessage("SAVE TODAY'S STRESS JOURNAL(00/00/0000)");
             ad.setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     try {
-                        FileOutputStream fout=openFileOutput(fileName.getText().toString()+".txt",MODE_PRIVATE);
+                        FileOutputStream fout = openFileOutput(fileName.getText().toString() + ".txt", MODE_PRIVATE);
                         fout.write(editText.getText().toString().getBytes());
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), "Error Occured:" + e, Toast.LENGTH_LONG).show();
@@ -65,37 +57,37 @@ public class journalPage extends AppCompatActivity {
             });
             ad.show();
         }
-             if (wow.getId()== R.id.openButton) {
-                 ad.setMessage("OPEN JOURNAL");
-                 ad.setPositiveButton("OPEN", new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialogInterface, int i) {
-                         int j;
-                         editText.setText("");
-                         try {
-                             FileInputStream fin = openFileInput(fileName.getText().toString()+".txt");
-                             while (( j = fin.read()) !=- 1) {
-                                 editText.setText((editText.getText().toString() + Character.toString((char) j)));
-                             }
+        if (wow.getId() == R.id.openButton) {
+            ad.setMessage("OPEN STRESS JOURNAL(00/00/0000)");
+            ad.setPositiveButton("OPEN", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    int j;
+                    editText.setText("");
+                    try {
+                        FileInputStream fin = openFileInput(fileName.getText().toString() + ".txt");
+                        while ((j = fin.read()) != -1) {
+                            editText.setText((editText.getText().toString() + Character.toString((char) j)));
+                        }
 
-                         }catch (Exception e) {
-                             Toast.makeText(getApplicationContext(), "Error Occured: " +e, Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error Occured: " + e, Toast.LENGTH_LONG).show();
 
-                         }
-                     }
-                 });
-                 ad.setNegativeButton("CANCEL",new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialog, int which) {
-                         dialog.cancel();
-                     }
-                 });
+                    }
+                }
+            });
+            ad.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
 
-                 ad.show();
-             }
+            ad.show();
+        }
 
-            if(wow.getId()==R.id.newButton) {
-                editText.setText("");
+        if (wow.getId() == R.id.newButton) {
+            editText.setText("");
         }
     }
 }
