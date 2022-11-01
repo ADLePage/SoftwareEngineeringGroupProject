0e634a884aca5dc5564b1aac16601acedf80b16c
ikotu002 <ikotu002@d.umn.edu>
2022-11-01 14:11:07 -0500

    DONE WITH THE JOURNAL PAGE

--- app/src/main/java/com/example/baads/journalPage.java
+++ app/src/main/java/com/example/baads/journalPage.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,56 +19,45 @@ public class journalPage extends AppCompatActivity {
      Button newButton,saveButton,openButton;
      EditText editText;
 
-     public void saveButtonFunction(){
-         final EditText fileName = new EditText(this);
-         AlertDialog.Builder ad = new AlertDialog.Builder(this);
-         ad.setView(fileName);
-         try {
-             FileOutputStream fout = openFileOutput(fileName.getText().toString() + ".txt", MODE_WORLD_READABLE);
-             fout.write(editText.getText().toString().getBytes());
-         } catch (Exception e) {
-             Toast.makeText(getApplicationContext(), "Error Occured:" + e, Toast.LENGTH_LONG).show();
-         }
-     }
-
-
-     @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_journal_page);
 
-        newButton = (Button) findViewById(R.id.newButton);
-        saveButton = (Button) findViewById(R.id.saveButton);
-        openButton = (Button) findViewById(R.id.openButton);
-        editText = (EditText) findViewById(R.id.text);
-
+        newButton = (Button)findViewById(R.id.newButton);
+        saveButton = (Button)findViewById(R.id.saveButton);
+        openButton = (Button)findViewById(R.id.openButton);
+        editText = (EditText)findViewById(R.id.text);
+/*
         saveButton.setOnClickListener(e->saveButtonFunction());
         saveButton.setText("Save Button");
         //saveButton.
+
+ */
     }
 
 
-/*
-    public void buttonAction(View v) {
+
+    public void buttonAction(View wow) {
         final EditText fileName = new EditText(this);
         AlertDialog.Builder ad = new AlertDialog.Builder(this);
         ad.setView(fileName);
-        if (v.getId() == R.id.saveButton) {
-            ad.setMessage("SAVE TODAY'S JOURNAL");
-            ad.setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
 
+        if (wow.getId() == R.id.saveButton)
+        {
+            ad.setMessage("SAVE TODAY'S JOURNAL");
+            ad.setPositiveButton("save", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     try {
-                        FileOutputStream fout = openFileOutput(fileName.getText().toString() + ".txt", MODE_WORLD_READABLE);
+                        FileOutputStream fout=openFileOutput(fileName.getText().toString()+".txt",MODE_PRIVATE);
                         fout.write(editText.getText().toString().getBytes());
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), "Error Occured:" + e, Toast.LENGTH_LONG).show();
                     }
                 }
             });
-            ad.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            ad.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.cancel();
@@ -76,8 +65,8 @@ public class journalPage extends AppCompatActivity {
             });
             ad.show();
         }
-             if (v.getId()== R.id.openButton) {
-                 ad.setMessage("OPEN THIS JOURNAL");
+             if (wow.getId()== R.id.openButton) {
+                 ad.setMessage("OPEN JOURNAL");
                  ad.setPositiveButton("OPEN", new DialogInterface.OnClickListener() {
                      @Override
                      public void onClick(DialogInterface dialogInterface, int i) {
@@ -85,7 +74,7 @@ public class journalPage extends AppCompatActivity {
                          editText.setText("");
                          try {
                              FileInputStream fin = openFileInput(fileName.getText().toString()+".txt");
-                             while (( j = fin.read()) !=-1) {
+                             while (( j = fin.read()) !=- 1) {
                                  editText.setText((editText.getText().toString() + Character.toString((char) j)));
                              }
 
@@ -95,7 +84,7 @@ public class journalPage extends AppCompatActivity {
                          }
                      }
                  });
-                 ad.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                 ad.setNegativeButton("CANCEL",new DialogInterface.OnClickListener() {
                      @Override
                      public void onClick(DialogInterface dialog, int which) {
                          dialog.cancel();
@@ -104,9 +93,9 @@ public class journalPage extends AppCompatActivity {
 
                  ad.show();
              }
-            if(v.getId()==R.id.newButton) {
-                editText.setText(" ");
+
+            if(wow.getId()==R.id.newButton) {
+                editText.setText("");
         }
     }
-*/
-    }
+}
