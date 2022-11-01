f131411c2a1bc3285c77cdefd96dbd4cd66f2d78
helge409 <helge409@d.umn.edu>
2022-11-01 15:09:31 -0500

    wiki and stress management
    Adding wiki button and page for stress management tips

---
+++ app/src/main/java/com/example/baads/StressManagement2.java
@@ -0,0 +1,25 @@
+package com.example.baads;
+
+//import static com.example.baads.R.id.wikiV;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class StressManagement2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stress_management2);
+
+       // newButton = (Button) findViewById(R.id.newButton);
+
+       WebView newWebView = (WebView) findViewById(R.id.wikiV);
+        setContentView(newWebView);
+        newWebView.loadUrl("https://www.wikihow.com");
+
+    }
+}
