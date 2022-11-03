d4c3584b3e92530b8225989cfda9750ca62f441a
lepag030 <lepag030@d.umn.edu>
2022-11-03 17:00:14 -0500

    Firebase realtime and Firestore Functionality.
    If issues with gradle, sync your gradle.
    To be able to use the firebase,
    1. Goto the tab on the top of your android studio named Tools and open it.
    2. Select Firebase.
    3. Theres a popup on the right of your screen, expand Cloud firestore or Realtime Database
    4. Click the get started link.
    5. Lower in the instructions, there will be examples of how to read and write to it.

--- app/src/main/java/com/example/baads/MainPage.java
+++ app/src/main/java/com/example/baads/MainPage.java
@@ -1,6 +1,9 @@
 package com.example.baads;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,19 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.baads.databinding.FragmentMainpageBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainPage extends Fragment {
 
@@ -88,7 +104,58 @@ public class MainPage extends Fragment {
             }
         });
 
+        /*
+        //ALL CODE FROM THIS POINT ON IS FROM THE FIREBASE TOOL FOR ANDROID STUDIO.
 
+        //Firebase Connection to the realtime database.
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        //Uncomment this to send a value to the database
+        //myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        //Firebase connection to the firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+        */
     }
 
     @Override
