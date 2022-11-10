87d0deeab442aa834247baba20b2f276f769c013
helge409 <helge409@d.umn.edu>
2022-11-10 14:54:29 -0600

    adds back login and youtube functionality

--- app/src/main/java/com/example/baads/MainPage.java
+++ app/src/main/java/com/example/baads/MainPage.java
@@ -46,19 +46,13 @@ public class MainPage extends Fragment {
             }
         });
         //Creates a binding to send user to the music player
-        binding.SoothingMusic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(MainPage.this)
-                        .navigate(R.id.action_FirstFragment_to_musicPlayer);
-            }
-        });
+
         //Creates a binding to send user to the youtube page
-        binding.YouTubePlayer.setOnClickListener(new View.OnClickListener() {
+        binding.youTubeVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(MainPage.this)
-                        .navigate(R.id.action_FirstFragment_to_youtubePlayer);
+                        .navigate(R.id.action_FirstFragment_to_youTubeVideo);
             }
         });
 
