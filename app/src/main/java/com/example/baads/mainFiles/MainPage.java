e6d494b0c68fe1f61c677ac9b4b115b7d861d986
lepag030 <lepag030@d.umn.edu>
2022-11-15 13:03:10 -0600

    New Update

--- app/src/main/java/com/example/baads/mainFiles/MainPage.java
+++ app/src/main/java/com/example/baads/mainFiles/MainPage.java
@@ -116,6 +116,14 @@ public class MainPage extends Fragment {
             }
         });
 
+        binding.stressRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MainPage.this)
+                        .navigate(R.id.action_FirstFragment_to_ratingInput);
+            }
+        });
+
     }
 
     @Override
