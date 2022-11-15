0314bd0ed7b3d1fe3dc6265455165350fc40a896
ikotu002 <ikotu002@d.umn.edu>
2022-11-15 10:52:27 -0600

    firebase firestore done for the daily activity page

--- app/src/main/java/com/example/baads/dailyActivity.java
+++ app/src/main/java/com/example/baads/dailyActivity.java
@@ -1,84 +1,105 @@
 package com.example.baads;
-
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.widget.Button;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.baads.databinding.ActivityDailyBinding;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
 
-import java.security.PrivateKey;
-import java.text.DateFormat;
-import java.util.Date;
+
+import com.example.baads.dailyAdapter.dailyActivityAdapter;
+import com.example.baads.dailyModel.dailyactivityModel;
+import com.google.android.gms.ads.mediation.Adapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class dailyActivity extends AppCompatActivity {
 
-   private Toolbar toolbar;
-   private RecyclerView recyclerView;
-   private FloatingActionButton floatingActionButton;
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
     private String task;
     private String description;
-   private DatabaseReference reference;
-   private String key  = "";
-
-
+    private DatabaseReference reference;
+    private String key = "";
+    private FirebaseFirestore firestore;
+    private dailyActivityAdapter adapter;
+    private List<dailyactivityModel> mlist;
 
     private ProgressDialog loader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_daily);
- toolbar= findViewById(R.id.dailytoolbar);
- setSupportActionBar(toolbar);
- getSupportActionBar().setTitle("your daily activities  :)");
-recyclerView = findViewById(R.id.recyclerView);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setReverseLayout(true);
-        linearLayoutManager.setStackFromEnd(true);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        loader = new ProgressDialog(this);
-        reference = FirebaseDatabase.getInstance().getReference().child("today activity ");
-
-
+        toolbar = findViewById(R.id.dailytoolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("your daily activities  :)");
+        recyclerView = findViewById(R.id.recyclerView);
         floatingActionButton = findViewById(R.id.fab);
+        firestore = FirebaseFirestore.getInstance();
+
+
+        //  LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //  linearLayoutManager.setReverseLayout(true);
+        //   linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        // recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setLayoutManager(new LinearLayoutManager(dailyActivity.this));
+        //  loader = new ProgressDialog(this);
+        //  reference = Firebase
+        //  reference = FirebaseDatabase.getInstance().getReference().child("today activity ");
+
+
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addTask();
+                addNewActivity.newInstance().show(getSupportFragmentManager(), addNewActivity.TAG);
             }
         });
-
-
+        mlist = new ArrayList<>();
+        adapter = new dailyActivityAdapter(dailyActivity.this, mlist);
+        recyclerView.setAdapter(adapter);
+        showData();
     }
+
+    private void showData() {
+     firestore.collection("daily activity").addSnapshotListener(new EventListener<QuerySnapshot>() {
+         @Override
+         public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+             for (DocumentChange documentChange : value.getDocumentChanges()) {
+                 if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                     String id = documentChange.getDocument().getId();
+                     dailyactivityModel dailyactivitymodel = documentChange.getDocument().toObject(dailyactivityModel.class).withId(id);
+                    mlist.add(dailyactivitymodel);
+                    adapter.notifyDataSetChanged();
+                 }
+             }
+             Collections.reverse(mlist);
+         }
+
+     });
+    }
+
+  /*
     private void addTask() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         LayoutInflater inflater = LayoutInflater.from(this);
@@ -86,10 +107,10 @@ recyclerView = findViewById(R.id.recyclerView);
         dialog.setView(myview);
         AlertDialog dialog1 = dialog.create();
         dialog1.setCancelable(false);
-        final EditText task =myview.findViewById(R.id.task);
+        final EditText task = myview.findViewById(R.id.task);
         final EditText description = myview.findViewById(R.id.des);
         Button save = myview.findViewById(R.id.saveButton2);
-        Button cancel =myview.findViewById(R.id.cancelButton2);
+        Button cancel = myview.findViewById(R.id.cancelButton2);
         save.setOnClickListener((v) -> {
             String myTask = task.getText().toString().trim();
             String myDescription = description.getText().toString().trim();
@@ -99,35 +120,8 @@ recyclerView = findViewById(R.id.recyclerView);
                 task.setError("Task Required");
                 return;
             }
-            if (TextUtils.isEmpty(myDescription)) {
-                description.setError("Description Required");
-                return;
-            } else {
-                loader.setMessage("putting in your data ");
-              //  loader.setCanceledOnTouchOutside(false);
-              //  loader.show();
-                Model model = new Model(myTask, myDescription, id, date);
-                reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                      if (task.isSuccessful()) {
-                          Toast.makeText(dailyActivity.this, "Daily activity has been inserted successfully", Toast.LENGTH_SHORT).show();
-                      }
-                      else
-                      {
-                          String error = task.getException().toString();
-                          Toast.makeText(dailyActivity.this, "daily activity" + error, Toast.LENGTH_SHORT).show();
-                         loader.dismiss();
-                      }
-                    }
-                });
-            }
-
-            dialog1.dismiss();
         });
-        dialog1.show();
-
     }
 
-
+   */
 }
---
+++ app/src/main/java/com/example/baads/dailyModel/dailyactivityModel.java
@@ -0,0 +1,18 @@
+package com.example.baads.dailyModel;
+
+public class dailyactivityModel  extends dailyID{
+    private String dailyActivity , due;
+    private int status;
+
+    public String getdailyActivity() {
+        return dailyActivity;
+    }
+
+    public String getDue() {
+        return due;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
