06affb862c62095891e2a093b8d380f7197c2b39
helge409 <helge409@d.umn.edu>
2022-11-16 14:40:46 -0600

    adds selectable to selfCareList

--- app/src/main/java/com/example/baads/selfCareList/selfCareListReworkedFragment.java
+++ app/src/main/java/com/example/baads/selfCareList/selfCareListReworkedFragment.java
@@ -4,14 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.baads.R;
 import com.example.baads.databinding.ActivitySelfCareListBinding;
+import com.example.baads.mainFiles.LoginFragment;
+import com.example.baads.mainFiles.usernameStorage;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class selfCareListReworkedFragment extends Fragment {
 
+    private FirebaseFirestore databaseLoginInfoConnection;
     private ActivitySelfCareListBinding binding;
+    //DocumentReference savedIdeasRef = db.collection("Practice mindfulness").document("SavedIdeasList");
+
+    String tips[]
+            = { "Practice mindfulness", "Take a break",
+            "Play video games", "Listen to music",
+            "Read a book", "Listen to a podcast",
+            "Reflect on things you are grateful for",
+            "Eat a healthy meal", "Engage in exercise",
+            "Go for a walk", "Drink water", "Practice good sleep hygiene",
+            "Call/text a friend", "Connect with nature",
+            "Meditate"};
+
 
     @Override
     public View onCreateView(
@@ -26,6 +52,31 @@ public class selfCareListReworkedFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        databaseLoginInfoConnection = FirebaseFirestore.getInstance();
+
+        ArrayAdapter<String> tasks = new ArrayAdapter<String>(getActivity(),
+                R.layout.selfcarelist_item_view, R.id.itemTextView,
+                tips);
+
+
+        ListView listTasks = (ListView) getActivity().findViewById(R.id.listView);
+        listTasks.setAdapter(tasks);
+/*
+        binding.listView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Map<String, Object> data1 = new HashMap<>();
+
+                data1.put("idea1", "Practice mindfulness");
+                databaseLoginInfoConnection.collection("users")
+                        .document(usernameStorage.username)
+                        .collection("SavedIdeas")
+                        .document("SavedIdeasList")
+                        .set(data1);
+            }
+        });
+       //*/
+
 
     }
 
