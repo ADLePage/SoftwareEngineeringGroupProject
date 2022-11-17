0e74223619c7455d22c41e1a331c4c40671f9f8f
Sydney Hutchens <hutch548@d.umn.edu>
2022-11-17 14:02:13 -0600

    basic agenda page functionality working
    push to firebase functionality is working

--- app/src/main/java/com/example/baads/agenda/AgendaReworkFragment.java
+++ app/src/main/java/com/example/baads/agenda/AgendaReworkFragment.java
@@ -3,6 +3,7 @@ package com.example.baads.agenda;
 import static com.example.baads.addNewActivity.TAG;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +19,12 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.baads.R;
 import com.example.baads.databinding.ActivityAgendaBinding;
 import com.example.baads.mainFiles.usernameStorage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Calendar;
 import java.util.HashMap;
@@ -54,44 +60,51 @@ public class AgendaReworkFragment extends Fragment {
         //initialize connection to data base
         databaseAgendaConnection = FirebaseFirestore.getInstance();
 
-        Calendar calendar = Calendar.getInstance();
-        Date actualTime = calendar.getTime();
-        date = actualTime.toString();
-
-        //textView
-        TextView EventText = getActivity().findViewById(R.id.EventListText);
-
-        //textEdit
-        EditText eventEditText = getActivity().findViewById(R.id.editEventText);
-        //button
-        Button sendEvent = getActivity().findViewById(R.id.newEventButton);
-       sendEvent.setOnClickListener(e->new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-
-
-
-               EditText eventEditText = getActivity().findViewById(R.id.editEventText);
-
-               event = eventEditText.getText().toString();
-
-               EventText.setText(eventEditText.getText().toString());
-
-
-               date = "10000";
-               //date = actualTime.toString();
-
-               Map<String, Object> data1 = new HashMap<>();
-               data1.put("agendaEvent", event);
-
-               databaseAgendaConnection.collection("users")
-                       .document(usernameStorage.username)
-                       .collection("Agenda")
-                       .document("TheBig")
-                       .set(data1);
-           }
+        CollectionReference callref = databaseAgendaConnection
+                .collection("users")
+                .document(usernameStorage.username)
+                .collection("Agenda");
+        callref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
         });
 
+
+
+        //button
+        Button sendEvent = getActivity().findViewById(R.id.newEventButton);
+        sendEvent.setOnClickListener(e -> saveEvent());
+    }
+    public void saveEvent() {
+                //editText
+                EditText eventEditText = getActivity().findViewById(R.id.editEventText);
+                //stores user input in a string variable to be later added to database
+                event = eventEditText.getText().toString();
+
+                //textView
+                TextView EventText = getActivity().findViewById(R.id.EventListText);
+                EventText.setText(eventEditText.getText().toString());
+                //gets the current time upon button press
+                Calendar calendar = Calendar.getInstance();
+                Date actualTime = calendar.getTime();
+                date = actualTime.toString();
+
+                Map<String, Object> data1 = new HashMap<>();
+                data1.put("agendaEvent", event);
+
+                databaseAgendaConnection.collection("users")
+                        .document(usernameStorage.username)
+                        .collection("Agenda")
+                        .document(date)
+                        .set(data1);
     }
 
     @Override
