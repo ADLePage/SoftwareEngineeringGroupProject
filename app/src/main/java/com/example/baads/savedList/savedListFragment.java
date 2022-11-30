7dd5deed92bfabab0cc4317c1f96caa0608ce67d
helge409 <helge409@d.umn.edu>
2022-11-30 15:36:36 -0600

    cleaned up code and added to list

--- app/src/main/java/com/example/baads/savedList/savedListFragment.java
+++ app/src/main/java/com/example/baads/savedList/savedListFragment.java
@@ -58,12 +58,7 @@ public class savedListFragment extends Fragment {
             "Call-text a friend", "Connect with nature",
             "Meditate", "Treat yourself", "Maintain a routine", "Take deep breaths"};
     ArrayList<String> savedArrayList = new ArrayList<>();
-          //  = {
-         //   "0"//, "1", "2"
-    //};
-    String value;
-    //List<ApiFuture<WriteResult>> saves = new ArrayList<>();
-           // int countVariable;
+
 
 
     @Override
@@ -81,176 +76,44 @@ public class savedListFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         databaseLoginInfoConnection = FirebaseFirestore.getInstance();
-        //TextView retrieveDataText = getActivity().findViewById(R.id.retrieveData);
-
-        String[] result = new String[1];
-        result[0] ="";
-       // savedArrayList.add("Call-text a friend");
-
         CollectionReference colRef = databaseLoginInfoConnection.collection("users")
                 .document(usernameStorage.username)
                 .collection("SavedIdeas");
-/*
-                colRef.addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                        savedArrayList.add("test");
-                        savedArrayList.add(String.valueOf(value.getDocuments()));
-                    }
-                });
-*/        ArrayAdapter<String> tasks = new ArrayAdapter<>(getActivity(),
+
+        ArrayAdapter<String> tasks = new ArrayAdapter<>(getActivity(),
                 R.layout.selfcarelist_item_view, R.id.itemTextView,
                 savedArrayList);
-       // tasks.notifyDataSetChanged();
 //set to tips for full list
 //set to savedArrayList for saved
 
-
         ListView listTasks = getActivity().findViewById(R.id.listView);
-
         listTasks.setAdapter(tasks);
-       // tasks.notifyDataSetChanged();
-
 
 
                 colRef.get()
-                .addOnCompleteListener(task -> {
-                   // savedArrayList.add("0");
+                .addOnCompleteListener(task -> {;
                         if (task.isSuccessful()) {
-                            int counter = 0;
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
-                               // QuerySnapshot document2 = task.getResult();
 
-                                savedArrayList.add(document.getId()); //FAILS PROGRAM
+                                savedArrayList.add(document.getId());
+                                //adds doc ID for each in collection
                                 tasks.notifyDataSetChanged();
-                                //requestLayout();
-                                //notifyDataSetChanged();
-                              //  savedArrayList.add("1");
-                                counter ++;
-                              //  int numInt = Integer.parseInt(document.getData().get("Idea").toString().replaceAll("[\\D]",""));
-                                // datapoint ...
-                               // result[0] += document.getData().get("Idea") + "\n";
-                                //result[0] += document.getData().values().toString();
+                                //notifies adapter of change in ListView
 
-                                //savedArrayList.add(String.valueOf(document2));
-                                //if (document2.exists()) {
-                                   // saved[0] = document2.toString();
-                                //value = document.getData().values().toString();
-
-                               //value = task.getResult().toString();
-
-                                //QuerySnapshot value1 = task.getResult();
-                                //DocumentSnapshot value2 = (DocumentSnapshot) value1;
-
-                                //value = task.getResult().getData().values().toString();
                                if (document.exists()) {
-                                    //value = task.getResult().getDocuments().toString();
-                                    //value = task.get();
-                                  // savedArrayList.add(document.getId().toString());
-                                  // value = document.getData().values().toString();
-                                  // savedArrayList.add(value);
-                                   //savedArrayList.add("!!!!!!!");
+
                                 } else {
                                     Log.d(TAG, "No such document");
-                                  // savedArrayList.add("AAAAAHHHHHH");
                                 }
-
-
-                                // }
-                            }   // _/
-                           // retrieveDataText.setText(result[0]);
-                            //savedArrayList.add(result[0]);
+                            }
                         } else {
                                 Log.d(TAG, "Error getting documents: ", task.getException());
-                           // savedArrayList.add("UNSUCCESSFUL");
                             }
-                        //} else {
-                         //   Log.d(TAG, "get failed with ", task.getException());
-                         //
                     });
 
-                //AHHHHHH */
-
-                /*
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-
-
-
-
-        //savedArrayList.add(task.getData().values().toString());
-       // savedArrayList.add("2");
-
-
-
-        //document(affirmation[0])
-        //savedArrayList.add(newValue.getResult().getData().values().toString());
-        //savedArrayList.add(newValue.getResult().getId().toString());
-        //savedArrayList.add(newValue.getResult().getId());
 
         String [] saved = savedArrayList.toArray(new String[savedArrayList.size()]);
-
-        /*
-        final QuerySnapshot<Map<String, dynamic>> docQuery =
-        await databaseLoginInfoConnection.collection("users")
-                .document(usernameStorage.username)
-                .collection("SavedIdeas")
-                .get();
-    */
-
-
-      /*  async getMarker() {
-    const snapshot = databaseLoginInfoConnection.collection("users")
-                    .document(usernameStorage.username)
-                    .collection("SavedIdeas")
-                    .get();
-            return snapshot.docs.map(doc => doc.data());
-        }
-*/
-            //saved[0] = task[0];
-           // saved[0] = String.valueOf(sList);
-
-
-
-//https://firebase.google.com/docs/firestore/query-data/get-data#java_8
-
-        /*
-        ApiFuture<QuerySnapshot> saved =  databaseLoginInfoConnection.collection("users")
-                .document(usernameStorage.username)
-                .collection("SavedIdeas").get();
-
-         */
-/*
-        CollectionReference colRef = databaseLoginInfoConnection.collection("users")
-                .document(usernameStorage.username)
-                .collection("SavedIdeas");
-    Query query = colRef;
-    AggregateQuery count = query.count();
-        count.get(AggregateSource.SERVER).addOnCompleteListener(task ->{
-            if(task.isSuccessful()){
-                AggregateQuerySnapshot snapshot = task.getResult();
-                countVariable = (int) snapshot.getCount();
-            }
-        });
-        //Try catch for thread. necessary in-case interrupted. Also necessary as it takes some time to pull.
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        for (int i = 0; i < countVariable; i++){
-           tips[i] = databaseLoginInfoConnection.collection("users")
-                   .document(usernameStorage.username)
-                   .collection("SavedIdeas")
-                   .document(i);
-        }
-
-*/
 
         listTasks.setOnItemClickListener((adapter, v, position, id) -> {
             String item = adapter.getItemAtPosition(position).toString();
@@ -261,64 +124,6 @@ public class savedListFragment extends Fragment {
                     .document(item)
                     .delete();
         });
-       /*
-        listTasks.setOnItemClickListener((adapter, v, position, id) -> {
-            String item = adapter.getItemAtPosition(position).toString();
-            DocumentReference docIdRef = databaseLoginInfoConnection.collection("users")
-                    .document(usernameStorage.username)
-                    .collection("SavedIdeas")
-                    .document(item);
-
-            docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-
-                        DocumentSnapshot document = task.getResult();
-                        if (document.exists()) {
-                            Log.d(TAG, "Document exists!");
-                            Map<String, Object> data1 = new HashMap<>();
-                            databaseLoginInfoConnection.collection("users")
-                                    .document(usernameStorage.username)
-                                    .collection("SavedIdeas")
-                                    .document(item)
-                                    .delete();
-                        } else {
-                            Log.d(TAG, "Document does not exist!");
-                        }
-                    } else {
-                        Log.d(TAG, "Failed with: ", task.getException());
-                    }
-                }
-            });
-
-        });
-*/
-        //Unused stuff
-        //DocumentReference docRef = databaseLoginInfoConnection.collection("SavedIdeas").document("SavedIdeasList");
-        //CollectionReference colRef = databaseLoginInfoConnection.collection("SavedIdeas");
-
-
-        /*
-                binding.listView.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        Map<String, Object> data1 = new HashMap<>();
-
-                        data1.put("idea1", "Practice mindfulness");
-                        databaseLoginInfoConnection.collection("users")
-                                .document(usernameStorage.username)
-                                .collection("SavedIdeas")
-                                .document("SavedIdeasList")
-                                .set(data1);
-                    }
-                });
-               //*/
-
-        //.document(item.toString()).set(colRef);
-        // Set the "isCapital" field of the city 'DC'
-        // docRef
-        //        .update("Take a break", true);
-        //   .add(field: item)
     }
 
 
